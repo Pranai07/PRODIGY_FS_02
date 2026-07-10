1813615b21a703769ecdac42285bfb03c1bcbcf5
Pranai07 <pranaisai007@gmail.com>
2026-07-11 03:55:02 +0530

    /api route fixed

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://employee-management-system-qw19.onrender.com/",
+  baseURL: "https://employee-management-system-qw19.onrender.com/api",
   withCredentials: true,
 });
 
