bb7a704576ab9356bc9a339de13e47a4c073d1b4
Pranai07 <pranaisai007@gmail.com>
2026-07-12 22:55:15 +0530

    Reset migrations

--- server/prisma/migrations/20260712172433_init/migration.sql
+++ server/prisma/migrations/20260712172433_init/migration.sql
@@ -1,19 +1,7 @@
-/*
-  Warnings:
-
-  - You are about to drop the `users` table. If the table is not empty, all the data it contains will be lost.
-
-*/
--- DropTable
-DROP TABLE "public"."users";
-
--- DropEnum
-DROP TYPE "public"."Role";
-
 -- CreateTable
 CREATE TABLE "public"."User" (
     "id" SERIAL NOT NULL,
-    "name" TEXT NOT NULL,
+    "fullName" TEXT NOT NULL,
     "email" TEXT NOT NULL,
     "password" TEXT NOT NULL,
     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
